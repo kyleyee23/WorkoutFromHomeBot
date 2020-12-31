b994f8a2997f3c2854d32ff47abc1e4afb22e143
kyle <kyle.g.yee@gmail.com>
2020-12-30 18:57:44 -0800

    update chestk workout reps

--- bot/chest.py
+++ bot/chest.py
@@ -5,7 +5,7 @@ workouts = [
   "Decline Push-Ups (15x/60s), 2 sets",
   "Tricep Pulldowns (15x/40s), 2 sets",
   "Bench Dips (15x/40s), 2 sets",
-  "Tricep Extensions (15x/60s), 2 sets",
+  "Tricep Extensions (10x/60s), 2 sets",
   "Explosive Negatives (10x/60s), 2 sets"
 ]
 
